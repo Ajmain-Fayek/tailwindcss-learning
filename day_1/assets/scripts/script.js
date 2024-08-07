7be61bf08cf18516c3714ee7364f1d87cead82fa
Ajmain Fayek <fayekajmain0@gmail.com>
2024-08-08 04:27:06 +0600

    basic URL setup complete for learning days dorectories

---
+++ day_1/assets/scripts/script.js
@@ -0,0 +1,11 @@
+// Day 2
+const day_2 = document.getElementById("day_2");
+day_2.addEventListener('click', function(){
+    window.location = "../day_2/index.html";
+})
+
+//Day 3
+const day_3 = document.getElementById("day_3");
+day_3.addEventListener('click', function(){
+    window.location = "../day_3/index.html";
+})
