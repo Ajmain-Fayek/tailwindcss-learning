7e489b84c6649233ab0160c5967187701e9f3bf9
Ajmain Fayek <fayekajmain0@gmail.com>
2024-08-09 22:07:24 +0600

    Day-2 : Hero section is complete

--- day_2/tailwind.config.js
+++ day_2/tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 75%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        darkGrayishBlue: 'hsl(227, 12%, 50%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
