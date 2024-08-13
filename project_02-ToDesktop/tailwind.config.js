af1bb1d5ff75e27ed1baafc7c221397db7d8cd23
Ajmain Fayek <fayekajmain0@gmail.com>
2024-08-13 11:49:19 +0600

    breakpoints are being set

--- project_02-ToDesktop/tailwind.config.js
+++ project_02-ToDesktop/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // => @media (min-width: 640px) { ... }
+        'sm': '480px',
+  
+        // => @media (min-width: 768px) { ... }
+        'md': '768px',
+  
+        // => @media (min-width: 976px) { ... }
+        'lg': '976px',
+  
+        // => @media (min-width: 1280px) { ... }
+        'xl': '1280px',
+  
+        // => @media (min-width: 1536px) { ... }
+        '2xl': '1536px',
+      },
+    },
   },
   plugins: [],
 }
