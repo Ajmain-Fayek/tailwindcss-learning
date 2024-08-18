d4dd01dc57f665ffb8a722ce2dca20365a267c05
Ajmain Fayek <fayekajmain0@gmail.com>
2024-08-18 09:37:20 +0600

    Project 3 - Tea House added to the landing page

--- project_00/assets/scripts/script.js
+++ project_00/assets/scripts/script.js
@@ -1,11 +1,17 @@
-// Day 2
-const day_2 = document.getElementById("day_2");
-day_2.addEventListener('click', function(){
+// Project 1
+const day_1 = document.getElementById("day_1");
+day_1.addEventListener('click', function(){
     window.location = "../project_01-Manage/index.html";
 })
 
-//Day 3
+//Project 2
+const day_2 = document.getElementById("day_2");
+day_2.addEventListener('click', function(){
+    window.location = "../project_02-ToDesktop/index.html";
+})
+
+//Project 3
 const day_3 = document.getElementById("day_3");
 day_3.addEventListener('click', function(){
-    window.location = "../project_02-ToDesktop/index.html";
+    window.location = "../project_03-Tea-House/index.html";
 })
